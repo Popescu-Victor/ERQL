cba91876b920aab5807ed343c68c99c198a2abad
Popescu-Victor <romaniahasmetal@gmail.com>
2026-03-13 13:16:54 +0200

    Update parser.rs

--- parser.rs
+++ parser.rs
@@ -5,7 +5,15 @@ fn split_to_lowercase(s: &str) -> Vec<String> {
 }
 
 fn main() {
-    let input = "file>upload>return>graph";
-    let result = split_to_lowercase(input);
-    println!("{:?}", result);
+let input = "file>upload>return>graph>scatter>prof>stud"; // This is just an example of a user input
+let result: Vec<String> = split_to_lowercase(input);
+    
+
+let subject = result.get(0).unwrap();
+let verb = result.get(1).unwrap();
+let modif = result.get(2..).unwrap();
+
+println!("{:?}", subject);
+println!("{:?}", verb);
+println!("{:?}", modif);
 }
