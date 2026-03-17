57ad6cfa112ed1bb17c35c0a473434a6136ba683
Popescu-Victor <romaniahasmetal@gmail.com>
2026-03-17 09:07:28 +0200

    Update scraped_files_date_converter.py

--- scraped_files_date_converter.py
+++ scraped_files_date_converter.py
@@ -1,11 +1,21 @@
 import pandas as pd
 from dateutil import parser
+import os
 
+test_file = r""
 
-test_file = r"path\file.xlsx"
-dates = ['03. Nov 2025', '30. Oct 2025', '2025-11-05']
+df = pd.read_excel(test_file)
+dates = []
+
 
 print(test_file)
+
+for d in df.iloc[:, -1]:
+    if d == "Yesterday":
+        dates.append("2025-11-04")
+    else:
+        parsed = parser.parse(d)
+        dates.append(parsed.strftime("%Y-%m-%d"))
+
 for d in dates:
-    parsed = parser.parse(d)
-    print(parsed.strftime("%Y-%m-%d"))
+    print(parser.parse('2025-11-05') - parser.parse(d))
