2cd6d4db3da5a65f9f2c026a27fcf59e441f12e0
Popescu-Victor <romaniahasmetal@gmail.com>
2026-03-19 12:47:43 +0200

    Create entries_per_student.py

---
+++ utils/entries_per_student.py
@@ -0,0 +1,23 @@
+import os
+import pandas as pd
+
+
+hashmap = {}
+
+
+for x in list_of_clean_reports:
+    df = pd.read_excel(x)
+    for col in df.columns:
+            if df[col].astype(str).str.contains("Test Passes for Participant:", case=False, na=False).any():
+                user_col = df.columns.get_loc(col)
+                for x in df.iloc[:,user_col]:
+                    if len(x.split("Participant: ")) > 1:
+                        total_hw_entries += 1
+                        if x in hashmap:
+                            hashmap[x] += 1
+                        else:
+                            hashmap[x] = 1
+    
+for x, y in hashmap.items():
+    print(x,y)
+print("Total homework entries for this course: " + str(total_hw_entries))
