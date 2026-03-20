1201fe9fa763b96fe3e6a342e5542e5966b1275e
Popescu-Victor <romaniahasmetal@gmail.com>
2026-03-20 10:49:15 +0200

    Comment explaining why I split filename strings

--- utils/validate_files.py
+++ utils/validate_files.py
@@ -9,7 +9,7 @@ list_of_clean_reports = []
 
 for i, filename in enumerate(files, start=1):
     filepath = os.path.join(folder, filename)
-    if '-ro' not in filepath:
+    if '-ro' not in filepath: # The scraper extension that I'm using returns files in the following format [site name] + [site extension] + [date]. The '-ro-' split here is to separate the date from the other parts of the file name.
         pass
     else:
         ext = filepath.split('-ro-')[1]
