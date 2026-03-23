2b135a82c5fca8d4362319d5a48b422dbcb65784
Popescu-Victor <romaniahasmetal@gmail.com>
2026-03-23 13:14:02 +0200

    Removed useless tests

--- src/interpreter.py
+++ src/interpreter.py
@@ -9,20 +9,5 @@ class Render:
         self.verb = rendered[1]
         self.obj = rendered[2]
         self.mod = rendered[3:]
-        
 
 decode = Render(input_user)
-
-if decode.subj == "hw":
-    pass
-
-if decode.subj == "cv":
-    pass
-
-if decode.subj == "personal":
-    pass
-
-if decode.subj == "groups":
-    pass
-
-
