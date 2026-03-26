d7cea9a1221a6bdf02317eca2be4c63472c4fbba
Popescu-Victor <romaniahasmetal@gmail.com>
2026-03-26 12:01:51 +0200

    Transformed this into a function

--- utils/validate_files.py
+++ utils/validate_files.py
@@ -1,18 +1,23 @@
 import os
+import pathlib
 
 
-folder = r""
-files = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
-list_of_clean_reports = []
+def validate_files(filepath):
+
+    folder = pathlib.Path(filepath)
+    files = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
+    list_of_clean_reports = []
 
 
-for i, filename in enumerate(files, start=1):
-    filepath = os.path.join(folder, filename)
-    if '-ro' not in filepath: # The scraper extension that I'm using returns files in the following format [site name] + [site extension] + [date]. The '-ro-' split here is to separate the date from the other parts of the file name.
-        pass
-    else:
-        ext = filepath.split('-ro-')[1]
-        if len(ext) == 15:
-            list_of_clean_reports.append(filepath)
-        else:
+    for i, filename in enumerate(files, start=1):
+        filepath = os.path.join(folder, filename)
+        if '-ro' not in filepath: # The scraper extension that I'm using returns files in the following format [site name] + [site extension] + [date]. The '-ro-' split here is to separate the date from the other parts of the file name.
             pass
+        else:
+            ext = filepath.split('-ro-')[1]
+            if len(ext) == 15:
+                list_of_clean_reports.append(filepath)
+            else:
+                pass
+            
+    return list_of_clean_reports
