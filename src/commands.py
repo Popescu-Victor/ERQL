6bca3a76014dfeccdd86a0dd57f3ba2f5f809f78
Popescu-Victor <romaniahasmetal@gmail.com>
2026-03-31 10:56:56 +0300

    Add additional subject checks in command_handler

--- src/commands.py
+++ src/commands.py
@@ -7,3 +7,9 @@ def command_handler(Parsed_input):
         if Parsed_input.verb == "graph":
             if Parsed_input.obj[0] == "scatter":
                 erql.scatter_plot(erql.df, erql.hor, erql.ver)
+                
+    if Parsed_input.subject == "file":
+
+    if Parsed_input.subject == "homework":
+
+    If Parsed_input.subject == "group":
