24f3d8f548a21df8551cab3ac87921e1ab2a9367
Popescu-Victor <romaniahasmetal@gmail.com>
2026-04-17 11:34:34 +0300

    Create stats.py with statistical analysis functions
    Added functions for KNN, ANOVA, linear regression, and logistic regression.

---
+++ src/stats.py
@@ -0,0 +1,13 @@
+# Moved functions related to more complex statistical analysis into a different file.
+
+def knn():
+  pass
+
+def anova():
+  pass
+
+def lin_reg():
+  pass
+
+def log_reg():
+  pass
