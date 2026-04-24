8b85b3a7fe5830f3969942c9e6d185393cc9b12d
Popescu Victor <romaniahasmetal@gmail.com>
2026-04-24 14:09:41 +0300

    Added username and password class for storing login information to use in scraping

---
+++ scraping/webdriver.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+
+driver = webdriver.Edge()
+driver.get("https://example.com")
+
+print(driver.title)
+
+driver.quit()
---
+++ src/login_details.py
@@ -0,0 +1,5 @@
+class username_password:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.input_as_list = [self.username, self.password]
