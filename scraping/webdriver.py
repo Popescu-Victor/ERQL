53c5713f4fd9b1098f145401a0cc541147dc8a62
Popescu Victor <romaniahasmetal@gmail.com>
2026-05-13 08:26:34 +0300

    Fixed selenium login/user details inputted in console

--- scraping/webdriver.py
+++ scraping/webdriver.py
@@ -5,20 +5,24 @@ import time
 
 def hw_scrape(username, password):
     driver = webdriver.Edge() 
-
     driver.get("https://roarmy.adlunap.ro")
     time.sleep(2)
  
-    username = driver.find_element(By.ID, "username")  # change selector
-    username.send_keys(username)
+    username_field = driver.find_element(By.ID, "username")
+    username_field.send_keys(username)
 
-    password = driver.find_element(By.ID, "password")  # change selector
-    password.send_keys(password)
-
-    password.send_keys(Keys.RETURN)
+    password_field = driver.find_element(By.ID, "password")
+    password_field.send_keys(password)
+    password_field.send_keys(Keys.RETURN)
     
     time.sleep(5)
-
     print("Logged in (hopefully)")
-
     driver.quit()
+
+def get_login_info():
+    username_input = input("Write your username: ")
+    password_input = input("Write your password: ")
+    return username_input, password_input
+
+user_data = get_login_info()
+hw_scrape(user_data[0], user_data[1])
