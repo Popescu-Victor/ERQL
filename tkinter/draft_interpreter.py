b6458b6f7349bbfbf1b608a822876ebcbae6265c
Popescu_Victor <romaniahasmetal@gmail.com>
2026-05-17 20:19:00 +0300

    Testing match-case style of interpreter instead of if/elif for readability

--- tkinter/draft_interpreter.py
+++ tkinter/draft_interpreter.py
@@ -1,12 +1,13 @@
-from src import erql
-from utils import help
+#from src import erql
+#from utils import help
 
 # Compared if-statement to match-case statements.
 
 def interpret(user_input):
-    match user_input:
+    input_list = user_input.split(">")
+    match user_input[0]:
         case "stats":
-            pass
+            print(input_list[0] + ' and ' + input_list[1])
         case "file":
             pass
         case "clear":
@@ -19,3 +20,7 @@ def interpret(user_input):
             pass
         case "save":
             pass
+
+
+user_input = input("Write input here:   ")
+interpret(user_input=user_input)
