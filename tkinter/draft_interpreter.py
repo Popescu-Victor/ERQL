f0f1a944afa212b296e32d9eba12dc3462e47eb2
Popescu Victor <romaniahasmetal@gmail.com>
2026-05-19 08:22:24 +0300

    Deleted reduntant files

--- tkinter/draft_interpreter.py
+++ tkinter/draft_interpreter.py
@@ -5,7 +5,7 @@
 
 def interpret(user_input):
     input_list = user_input.split(">")
-    match user_input[0]:
+    match input_list[0]:
         case "stats":
             print(input_list[0] + ' and ' + input_list[1])
         case "file":
