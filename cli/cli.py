8d3fda712b5eec7e03d3699c0e75c8332cae0103
Popescu Victor <romaniahasmetal@gmail.com>
2026-06-19 11:26:47 +0300

    Setting up program in powershell

--- cli/cli.py
+++ cli/cli.py
@@ -1,22 +1,11 @@
 #!/usr/bin/env python3
 # cli.py
-
-import argparse
 import sys
 
-
-def parse_ui():
-    user_input = input("Write your ERQL script here: ")
-    commands = user_input.split(">")
-    return commands
-
-command_list = parse_ui()
-
-def interpret(commands):
-    if commands[0] == "file":
-        if commands[1] == "upload":
-            from tkinter import filedialog
-            file = filedialog.askopenfilename(title="Select a file to upload")
-            print(f"File uploaded: {file}")
-
-interpret(command_list)
+raw_input = sys.argv[1:]
+if len(raw_input) == 0:
+    print("No arguments provided. Please provide a command.")
+    sys.exit(1)
+else:
+    command = raw_input[0]
+    print(f"Command received: {command}")
