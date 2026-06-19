ac78f73b102e505e1649e0a1bc118ed0411a9fd9
Popescu Victor <romaniahasmetal@gmail.com>
2026-06-19 12:40:04 +0300

    Save filepath in memory from cli

---
+++ cli_main.py
@@ -0,0 +1,34 @@
+import argparse
+import sys
+import time
+from cli import *
+from src import file_in_memory
+
+# In $PROFILE, add the following line to make the script available in PowerShell:
+#   function erql {python "[path_to_script]" @args}
+
+def main():
+    raw_input = sys.argv[1:]
+    if len(raw_input) == 0:
+        print("No arguments provided. Please provide a command.")
+        sys.exit(1)
+    else:
+        command = raw_input[0].split('-')
+        return command
+
+command = main()
+
+def interpret(command):
+    if command[0] == 'file':
+        if command[1] == 'upload':
+            from tkinter import filedialog
+            from tkinter import Tk
+            file_path = filedialog.askopenfilename()
+            if file_path[-4:] == '.csv':
+                file_in_memory.Filepath(file_path)
+                print(f"File selected: {file_path}")
+            else:
+                print("No file selected or wrong file format. Please upload a valid .csv file")
+
+
+interpret(command)
