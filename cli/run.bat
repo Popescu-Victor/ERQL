b7d989b46ac31557d3b4ffb34a803af7436998dd
Popescu Victor <romaniahasmetal@gmail.com>
2026-06-22 10:58:22 +0300

    Comment explaining bat file

--- cli/run.bat
+++ cli/run.bat
@@ -1,3 +1,5 @@
+REM This is a batch file to run the CLI version of ERQL while not needing to have a Python environment set up. It assumes that Python 3 is installed and added to the system PATH.
+
 @echo off
 python3 cli.py
 pause
