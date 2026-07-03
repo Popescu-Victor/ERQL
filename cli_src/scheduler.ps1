1f4bb8788b258d8dd5a3a075f0f13a0a4a59eb65
Popescu Victor <romaniahasmetal@gmail.com>
2026-07-03 10:12:23 +0300

    Removed obsolete features: telegram bot/scraping/cli

--- cli_src/scheduler.ps1
+++
@@ -1,9 +0,0 @@
-$action = New-ScheduledTaskAction `
-    -Execute "C:\Users\popescu.victor\AppData\Local\Microsoft\WindowsApps\python.exe" `
-    -Argument "C:\Users\popescu.victor\Desktop\github\ERQL\cli\cli.py" `
-    -WorkingDirectory "C:\Users\popescu.victor\Desktop\github\ERQL\cli"
-
-
-$trigger = New-ScheduledTaskTrigger -Daily -At 8am
-
-Register-ScheduledTask -TaskName "DailyScraper" -Action $action -Trigger $trigger
