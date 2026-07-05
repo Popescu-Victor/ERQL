987398318807f29da7cc55ff30204f9d8b395f06
Popescu_Victor <romaniahasmetal@gmail.com>
2026-07-05 13:41:25 +0300

    Removed unaccessible tkinter import

--- src/graph_functions.py
+++ src/graph_functions.py
@@ -4,9 +4,8 @@ import pandas as pd
 import seaborn as sns
 from src import erql
 
-def scatter(df, x_axis, y_axis):
+def scatter(df, x_axis, y_axis, fig):
     alpha_level = erql.set_alpha_level(df.shape[0])
-    fig = Figure()
     ax = fig.add_subplot(111)
     sns.scatterplot(x=x_axis, y=y_axis, data=df, ax=ax, alpha=alpha_level)
     plt.show()
