d83ce190c6ff517ac4b81c1d9bf895affd55f168
Popescu-Victor <romaniahasmetal@gmail.com>
2026-07-08 09:18:53 +0300

    Add comment for storing login details in .env file
    Added comment explaining the use of .env file for login details.

--- cloud/firebase/login_to_env.py
+++ cloud/firebase/login_to_env.py
@@ -1,6 +1,6 @@
 import dotenv
 
-
+# When using ERQL to scrape the same page on a regular basis it's better to have login details stored in an .env file instead of rewriting them every time.
 
 def login_to_env(username, password):
     dotenv.set_key(".env", "ILIAS_USER", username)
